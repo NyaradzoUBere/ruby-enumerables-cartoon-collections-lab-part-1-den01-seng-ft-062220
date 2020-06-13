3b22d0f8d6f9566b6b9f98308e8047cf888d6b6a
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-13 04:43:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  greet_characters.each do |characters|
+  array.each do |characters|
     puts "Hello #{characters}!"
 end
 end
